2d026a78c98028196606929a7e82092e1e033584
Davi93 <dxa869@student.bham.ac.uk>
2021-10-22 17:10:46 +0100

    small change

--- fmri_signal_to_noise_ratio/signa_to_noise_ratio.py
+++ fmri_signal_to_noise_ratio/signa_to_noise_ratio.py
@@ -29,10 +29,8 @@ def sig_to_noise_nifti(input_path,ROI_path=None,save_output=False):
         
         #Resampling ROI to functional scan
         from nilearn.image import resample_to_img
-        ROI_image_resampled = resample_to_img(ROI_image,input_image,interpolation='nearest')     
-        
-        ROI_image_data = ROI_image_resampled.get_fdata()
-        ROI_image_data = np.where(ROI_image_data > 0, 1,np.nan)
+        ROI_image_resampled = resample_to_img(ROI_image,input_image,interpolation='nearest')            
+        ROI_image_data = np.where(ROI_image_resampled.get_fdata() > 0, 1,np.nan)
         
         #Applying ROI to input image
         for i in range(0,input_image_data.shape[3]):
