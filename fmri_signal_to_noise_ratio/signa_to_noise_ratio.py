b1f78d330cff1425d73fa507ebaecec31ec8bffe
Davi93 <dxa869@student.bham.ac.uk>
2021-10-22 17:12:29 +0100

    .

--- fmri_signal_to_noise_ratio/signa_to_noise_ratio.py
+++ fmri_signal_to_noise_ratio/signa_to_noise_ratio.py
@@ -30,12 +30,12 @@ def sig_to_noise_nifti(input_path,ROI_path=None,save_output=False):
         #Resampling ROI to functional scan
         from nilearn.image import resample_to_img
         ROI_image_resampled = resample_to_img(ROI_image,input_image,interpolation='nearest')            
-        ROI_image_data = np.where(ROI_image_resampled.get_fdata() > 0, 1,np.nan)
+        ROI_image_data = np.where(ROI_image_resampled.get_fdata() > 0, 1, np.nan)
         
         #Applying ROI to input image
         for i in range(0,input_image_data.shape[3]):
             print('Applying mask to volume: ' + str(i))
-            input_image_data[:,:,:,i] = np.multiply(input_image_data[:,:,:,i],ROI_image_data)
+            input_image_data[:,:,:,i] = np.multiply(input_image_data[:,:,:,i], ROI_image_data)
     
     #Calculating mean and standard deviation
     mean = np.nanmean(input_image_data,3)
