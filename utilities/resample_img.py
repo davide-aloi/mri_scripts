2dc80c19ce5346c3ec01eaa75475d135a741b82e
Davi93 <dxa869@student.bham.ac.uk>
2021-12-13 17:18:00 +0000

    some changes

---
+++ utilities/resample_img.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 13:29:38 2021
+
+@author: Davide Aloi, PhD student - University of Birmingham
+"""
+
+import os
+from nilearn import image
+
+main_folder = ""
+output_path = ""
+func_file = "" # func file
+mask = ""
+
+# func path
+func_map = os.path.join(main_folder, func_file) #path to func file
+
+# Loading func
+func_map = image.load_img(func_map) #load func file
+
+# mask path
+mask_map = os.path.join(main_folder,mask)
+
+# Loading mask
+mask_map = image.load_img(mask_map)
+
+
+from nilearn.image import resample_to_img
+mask_resampled = resample_to_img(mask_map,func_map)
+
+print('Your old mask has a shape of\n')
+print(mask_map.get_fdata().shape)
+
+print('Your functional file has a shape of\n')
+print(func_map.get_fdata().shape)
+
+print('Your new mask  has a shape of\n')
+print(mask_resampled.get_fdata().shape)
+
+#Saving everything
+
+image.math_img("img", img=mask_resampled).to_filename('output.nii.gz')
