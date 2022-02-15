785eddbf9be1b29a9549047a89cdf0a655f58ea0
Davi93 <dxa869@student.bham.ac.uk>
2022-02-15 09:35:36 +0000

    comments added

--- utilities/coord_matrix_mni.py
+++ utilities/coord_matrix_mni.py
@@ -20,11 +20,11 @@ def mni_to_matrix(mni_coords,T=None):
                       [ 0,     0,     0,     1]])
 
     first_arg = np.transpose(np.linalg.inv(T))
-    second_arg = (np.array([mni_coords[0],mni_coords[1],mni_coords[2], 1]));
+    second_arg = (np.array([mni_coords[0],mni_coords[1],mni_coords[2], 1]))
 
-    mat_coord = np.dot(second_arg,first_arg);
-    mat_coord = mat_coord[0:3];
-    mat_coord = np.round(mat_coord[:]);
+    mat_coord = np.dot(second_arg,first_arg)
+    mat_coord = mat_coord[0:3]
+    mat_coord = np.round(mat_coord[:])
 
     return mat_coord
 
@@ -38,7 +38,7 @@ def matrix_to_mni(matrix_coord,T=None):
                       [ 0,     0,     1,   -73],
                       [ 0,     0,     0,     1]])
 
-        second_arg = np.array([matrix_coord[0],matrix_coord[1],matrix_coord[2], 1]);
+        second_arg = np.array([matrix_coord[0],matrix_coord[1],matrix_coord[2], 1])
         second_arg = np.reshape(second_arg,[-1,1])
         mni_coord = np.dot(T,second_arg)
 
