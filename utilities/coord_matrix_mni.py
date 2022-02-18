57b7444942602eb150335c7ff1cc11422f918add
Davi93 <dxa869@student.bham.ac.uk>
2022-02-18 11:54:30 +0000

    matrix to mni script updated

--- utilities/coord_matrix_mni.py
+++ utilities/coord_matrix_mni.py
@@ -10,18 +10,11 @@ Please, remember that python indices from 0, as such, you have to add + 1 to the
 
 import numpy as np
 
-def mni_to_matrix(mni_coords,T=None):
+def mni_to_matrix(mni_coords, T):
     # From MNI space to Matrix space
-
-    if T==None:
-        T = np.array([[-1 ,   0,     0,    91],
-                      [ 0,     1,     0,  -127],
-                      [ 0,     0,     1,   -73],
-                      [ 0,     0,     0,     1]])
 
     first_arg = np.transpose(np.linalg.inv(T))
     second_arg = (np.array([mni_coords[0],mni_coords[1],mni_coords[2], 1]))
-
     mat_coord = np.dot(second_arg,first_arg)
     mat_coord = mat_coord[0:3]
     mat_coord = np.round(mat_coord[:])
@@ -29,15 +22,8 @@ def mni_to_matrix(mni_coords,T=None):
     return mat_coord
 
 
-def matrix_to_mni(matrix_coord,T=None):
+def matrix_to_mni(matrix_coord, T):
     # From matrix space to MNI space
-
-    if T == None:
-        T = np.array([[-1,   0,     0,    91],
-                      [ 0,     1,     0,  -127],
-                      [ 0,     0,     1,   -73],
-                      [ 0,     0,     0,     1]])
-
         second_arg = np.array([matrix_coord[0],matrix_coord[1],matrix_coord[2], 1])
         second_arg = np.reshape(second_arg,[-1,1])
         mni_coord = np.dot(T,second_arg)
